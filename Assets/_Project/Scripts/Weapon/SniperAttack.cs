ded7b39d33bf1157d1b5c415faceb7d9592b7644
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2023-03-27 12:44:38 +0200

    added sniper riffle

---
+++ Assets/_Project/Scripts/Weapon/SniperAttack.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SniperAttack : MonoBehaviour, IAttack
+{
+    [Tooltip("Declares how often player can shoot")]
+    [SerializeField] private float timeBetweenShooting;
+    [Tooltip("Declares range of bullet spawn, while player is moving")]
+    [SerializeField] private float spread;
+    private Weapon usedWeapon;
+    private PlayerInputController playerInputController;
+    private Vector2 movementAxis;
+
+    //TODO: Change this if we'll have reference manager
+    private void Awake()
+    {
+        playerInputController = FindObjectOfType<PlayerInputController>();
+    }
+
+    private void OnEnable()
+    {
+        playerInputController.movementEvent += OnMovementEvent;
+    }
+
+    private void OnDisable()
+    {
+        playerInputController.movementEvent -= OnMovementEvent;
+    }
+
+    public void Attack(Weapon weapon)
+    {
+        usedWeapon = weapon;
+        Shot();
+    }
+    
+    private void Shot()
+    {
+        usedWeapon.isLastShotOver = false;
+        usedWeapon.As.PlayOneShot(usedWeapon.As.clip);
+        
+        //Choose spread depending on player's controls
+        float chosenSpread = movementAxis == Vector2.zero ? 0 : Random.Range(-spread, spread);
+        
+        Instantiate(usedWeapon.BulletPrefab, usedWeapon.BulletsSpawnPoint.position, usedWeapon.transform.rotation).GetComponent<IBullet>().SetupBullet(chosenSpread);
+        
+        usedWeapon.BulletsLeft--;
+        usedWeapon.Invoke(nameof(usedWeapon.ResetShot), timeBetweenShooting);
+    }
+
+    private void OnMovementEvent(Vector2 dir)
+    {
+        movementAxis = dir;
+    }
+}
