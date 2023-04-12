da508983540d875059ca249409735bd829cbb5f7
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2023-04-12 13:43:43 +0200

    weapon script no longer detects inputs (#11)

--- Assets/_Project/Scripts/Weapon/Bat.cs
+++ Assets/_Project/Scripts/Weapon/Bat.cs
@@ -7,6 +7,11 @@ public class Bat : MonoBehaviour, IWeapon
         Debug.Log(name + "Primary attack");
     }
 
+    public void Reload()
+    {
+        
+    }
+
     public GameObject GetGameObject()
     {
         return gameObject;
