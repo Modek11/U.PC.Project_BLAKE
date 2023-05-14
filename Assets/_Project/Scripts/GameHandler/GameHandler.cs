cc1284fdc39feb10482d8fa0f40403675cf682a6
Modekk <modek11@op.pl>
2023-05-14 18:57:00 +0200

    Added controls popup

--- Assets/_Project/Scripts/GameHandler/GameHandler.cs
+++ Assets/_Project/Scripts/GameHandler/GameHandler.cs
@@ -21,28 +21,52 @@ public class GameHandler : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        ShowPlayerControlsPopup();
+    }
+
+    public void PlayerPause()
+    {
+        OpenPlayerUICanvas("PauseGame_Canvas");
+    }
+
     public void PlayerWin()
     {
         Time.timeScale = 0f;
-        for(int i = 0; i < pausedGameCanvas.transform.childCount; i++)
-        {
-            var child = pausedGameCanvas.transform.GetChild(i).gameObject;
-            child.SetActive(child.name is "BackgroundFade" or "YouWin_Canvas");
-        }
-
-        pausedGameCanvas.SetActive(true);
+        OpenPlayerUICanvas("YouWin_Canvas");
     }
 
     public void PlayerLose()
     {
         Time.timeScale = 0f;
+        OpenPlayerUICanvas("YouLose_Canvas");
+    }
+
+    public void ShowPlayerControlsPopup()
+    {
+        OpenPlayerUICanvas("ControlsPopup_Canvas");
+    }
+
+    private void OpenPlayerUICanvas(string canvasName)
+    {
         for(int i = 0; i < pausedGameCanvas.transform.childCount; i++)
         {
             var child = pausedGameCanvas.transform.GetChild(i).gameObject;
-            child.SetActive(child.name is "BackgroundFade" or "YouLose_Canvas");
+            child.SetActive(child.name == canvasName);
         }
-        
         pausedGameCanvas.SetActive(true);
+    }
+    
+    public void ClosePausedGameCanvas()
+    {
+        pausedGameCanvas.SetActive(false);
+        for(int i = 0; i < pausedGameCanvas.transform.childCount; i++)
+        {
+            var child = pausedGameCanvas.transform.GetChild(i).gameObject;
+            child.SetActive(false);
+        }
+        Time.timeScale = 1f;
     }
 
     private void OnDestroy()
