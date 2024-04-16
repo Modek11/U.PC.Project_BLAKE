87553aabecc84e411f48cff96db0d4d3256d1bf8
Modekk <modek11@op.pl>
2024-04-16 16:43:33 +0200

    Added StarterScene to load all needed singletons

---
+++ Assets/_Project/Scripts/SceneHandler/StarterScene.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.SceneHandler
+{
+    public class StarterScene : MonoBehaviour
+    {
+        [SerializeField] private bool instantLoadGame = true;
+        private float delayTimeBetweenLogs = .5f;
+
+        private void Start()
+        {
+            _ = StartLoading();
+        }
+
+        private async UniTaskVoid StartLoading()
+        {
+            var objectsInActiveScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects().ToList();
+            objectsInActiveScene.RemoveAll(go => go == gameObject);
+            
+            while (objectsInActiveScene.Count > 0)
+            {
+                Debug.Log($"There are still {objectsInActiveScene.Count} objects not loaded from StarterScene!");
+                await UniTask.Delay(TimeSpan.FromSeconds(delayTimeBetweenLogs));
+            }
+
+            if (instantLoadGame)
+            {
+                ReferenceManager.SceneHandler.StartNewGame();
+            }
+            else
+            {
+                ReferenceManager.SceneHandler.LoadMainMenu();
+            }
+        }
+    }
+}
