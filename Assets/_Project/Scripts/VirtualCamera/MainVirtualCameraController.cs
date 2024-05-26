89dcccdb11648f841f9433e39965b8b8cf29175b
Modekk <modek11@op.pl>
2024-05-27 00:24:57 +0200

    Zooming out now has his own reset time

--- Assets/_Project/Scripts/VirtualCamera/MainVirtualCameraController.cs
+++ Assets/_Project/Scripts/VirtualCamera/MainVirtualCameraController.cs
@@ -7,13 +7,15 @@ namespace _Project.Scripts.VirtualCamera
     public class MainVirtualCameraController : MonoBehaviour
     {
         [SerializeField] private float zoomOutValue;
-        [SerializeField] private float duration;
+        [SerializeField] private float zoomingDuration;
+        [SerializeField] private float zoomResetDuration;
         
         private CinemachineVirtualCamera _virtualCamera;
         private bool _isZoomedOut = false;
         private float _basicOrthoSize;
 
         private Tween _tween;
+        private Sequence _resetTween;
 
         private void Awake()
         {
@@ -25,16 +27,32 @@ namespace _Project.Scripts.VirtualCamera
         public void ChangeZoom()
         {
             _tween.Kill();
+            _resetTween.Kill();
+            _resetTween = DOTween.Sequence();
+            
             var from = _virtualCamera.m_Lens.OrthographicSize;
+            if (Mathf.Approximately(from, _basicOrthoSize))
+            {
+                _isZoomedOut = false;
+            }
+            
             var to = _isZoomedOut ? _basicOrthoSize : _basicOrthoSize + zoomOutValue;
-
-            _tween = DOVirtual.Float(from, to, duration, v => _virtualCamera.m_Lens.OrthographicSize = v);
+            
+            _tween = DOVirtual.Float(from, to, zoomingDuration, v => _virtualCamera.m_Lens.OrthographicSize = v);
+            if (!_isZoomedOut)
+            {
+                _resetTween.Append(DOVirtual.Float(to, _basicOrthoSize, zoomingDuration, v => _virtualCamera.m_Lens.OrthographicSize = v)
+                    .SetDelay(zoomingDuration + zoomResetDuration));
+                _resetTween.Append(DOVirtual.DelayedCall(0f, () => _isZoomedOut = false));
+            }
+            
             _isZoomedOut = !_isZoomedOut;
         }
 
         private void OnDisable()
         {
             _tween.Kill();
+            _resetTween.Kill();
         }
     }
 }
