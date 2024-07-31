8a5072c9aea181239f6720c2bab246e54c14f8ea
Modekk <modek11@op.pl>
2024-07-31 22:36:32 +0200

    Enemis now can detect player from behind or by collision

--- Assets/_Project/Scripts/EnemyAI/EnemyFOV.cs
+++ Assets/_Project/Scripts/EnemyAI/EnemyFOV.cs
@@ -1,68 +1,107 @@
-using System.Collections;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class EnemyFOV : MonoBehaviour
+namespace _Project.Scripts.EnemyAI
 {
-    public float Radius;
-    [Range(0, 360)] public float Angle;
-
-    public LayerMask TargetMask;
-    public LayerMask ObstacleMask;
-    public Transform raycastOrigin;
-
-    [HideInInspector]
-    public bool CanSeePlayer;
-
-    public delegate void CanSeePlayerChanged(bool newCanSeePlayer);
-    public event CanSeePlayerChanged OnCanSeePlayerChanged;
-
-    private void OnEnable()
+    public class EnemyFOV : MonoBehaviour
     {
-        StartCoroutine(FOVRoutine());
-    }
+        public float Radius;
+        [Range(0, 360)] public float Angle;
 
-    public void FieldOfViewCheck()
-    {
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, Radius, TargetMask);
+        [SerializeField] private LayerMask TargetMask;
+        [SerializeField] private LayerMask ObstacleMask;
+        [SerializeField] private Transform raycastOrigin;
+        [SerializeField] private float fovCheckDelay;
+        [SerializeField] private float maxBackVisibilityDistance;
+        [SerializeField] private float findPlayerFromBehindDelay;
+        
 
-        if (rangeChecks.Length != 0)
+        //create one Collider because there will be only one player
+        private Collider[] overlapSphereColliders = new Collider[1];
+
+        [HideInInspector]
+        public bool CanSeePlayer;
+
+        public delegate void CanSeePlayerChanged(bool newCanSeePlayer);
+        public event CanSeePlayerChanged OnCanSeePlayerChanged;
+
+        private void OnEnable()
         {
-            Transform target = rangeChecks[0].transform;
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, directionToTarget) < Angle / 2)
+            _ = FOVRoutine();
+        }
+        
+        private async UniTaskVoid FOVRoutine()
+        {
+            var cToken = this.GetCancellationTokenOnDestroy();
+            while (true)
             {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                await UniTask.Delay(TimeSpan.FromSeconds(fovCheckDelay), cancellationToken: cToken);
+                _ = FieldOfViewCheck(cToken);
+            }
+        }
 
-                if (!Physics.Raycast(raycastOrigin.position, directionToTarget, distanceToTarget, ObstacleMask))
+        private async UniTaskVoid FieldOfViewCheck(CancellationToken cToken)
+        {
+            Physics.OverlapSphereNonAlloc(transform.position, Radius, overlapSphereColliders, TargetMask);
+
+            if (overlapSphereColliders[0] is null)
+            {
+                if (CanSeePlayer)
                 {
-                    if (!CanSeePlayer)
-                    {
-                        CanSeePlayer = true;
-                        OnCanSeePlayerChanged?.Invoke(CanSeePlayer);
-                    }
-                    return;
+                    OnCanSeePlayerChanged?.Invoke(CanSeePlayer = false);
+                }
+                
+                return;
+            }
+
+            if (CanSeePlayer)
+            {
+                return;
+            }
+
+            var target = overlapSphereColliders[0].transform;
+            var directionToTarget = (target.position - transform.position).normalized;
+            var distanceToTarget = Vector3.Distance(transform.position, target.position);
+            
+            if (!Physics.Raycast(raycastOrigin.position, directionToTarget, distanceToTarget, ObstacleMask))
+            {
+                //front of enemy 
+                if (Vector3.Angle(transform.forward, directionToTarget) < Angle / 2)
+                {
+                    OnCanSeePlayerChanged?.Invoke(CanSeePlayer = true);
+                }
+                //back of enemy
+                else if(distanceToTarget < maxBackVisibilityDistance)
+                {
+                    CanSeePlayer = true;
+                    await UniTask.Delay(TimeSpan.FromSeconds(findPlayerFromBehindDelay), cancellationToken: cToken);
+                    OnCanSeePlayerChanged?.Invoke(CanSeePlayer);
                 }
             }
         }
-        if (CanSeePlayer)
+
+        private void OnCollisionEnter(Collision other)
         {
-            CanSeePlayer = false;
-            OnCanSeePlayerChanged?.Invoke(CanSeePlayer);
+            if (other.collider.CompareTag("Player"))
+            {
+                OnCanSeePlayerChanged?.Invoke(CanSeePlayer = true);
+            }
         }
 
-    }
-
-    private IEnumerator FOVRoutine()
-    {
-        float fovCheckDelay = 0.2f;
-
-        while (true)
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
         {
-            yield return new WaitForSeconds(fovCheckDelay);
-
-            FieldOfViewCheck();
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+            
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, maxBackVisibilityDistance);
+            Gizmos.DrawWireSphere(transform.position, Radius);
         }
-        
+#endif
     }
 }
